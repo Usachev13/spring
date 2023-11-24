a5e9b66358815002e1b3e7748518972007923e63
Usachev13 <2893318@gmail.com>
2023-11-24 18:15:00 +0200

    hw start

---
+++ src/main/java/pro/skySpring/skyspring/SkySpringApplication.java
@@ -0,0 +1,13 @@
+package pro.skySpring.skyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySpringApplication.class, args);
+	}
+
+}
